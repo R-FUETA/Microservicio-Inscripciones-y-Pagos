2e4a4c7d9521c227943f5e78061475f769d41252
R-FUETA <raul.a.fuenzalida@gmail.com>
2025-06-23 12:26:16 -0400

    1.1.4

--- src/main/java/com/edutech_innovators_spa/cl/edutech_innovators_spa/inscripciones/controller/InscripcionController.java
+++ src/main/java/com/edutech_innovators_spa/cl/edutech_innovators_spa/inscripciones/controller/InscripcionController.java
@@ -2,6 +2,10 @@ package com.edutech_innovators_spa.cl.edutech_innovators_spa.inscripciones.contr
 
 import com.edutech_innovators_spa.cl.edutech_innovators_spa.inscripciones.model.Inscripcion;
 import com.edutech_innovators_spa.cl.edutech_innovators_spa.inscripciones.service.InscripcionService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,10 +13,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/inscripciones")
+@Tag(name = "Inscripciones", description = ("Permite Inscribir a los estudiantes y Visualizar los estudiantes Inscritos en los cursos"))
 public class InscripcionController {
     @Autowired
     private InscripcionService inscripcionService;
-
+    @Operation(summary = "Inscribe al Estudiante", description = "Permite Inscribir al Estudiante Al curso")
     @PostMapping
     public Inscripcion inscribir(@RequestParam Long idCurso,
                                   @RequestParam Long idEstudiante,
@@ -20,7 +25,7 @@ public class InscripcionController {
                                   @RequestParam(required = false) String cupon) {
         return inscripcionService.registrarInscripcion(idCurso, idEstudiante, monto, cupon);
     }
-
+    @Operation(summary = "Estudiantes Inscritos", description = "Permite Visualizar Los estudiantes Inscritos en los Cursos")
     @GetMapping("/estudiante/{id}")
     public List<Inscripcion> listarPorEstudiante(@PathVariable Long id) {
         return inscripcionService.listarPorEstudiante(id);
